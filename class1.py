1595ff3d575764d4f1841c264b1f6e872a798304
Nilgun Tapkan <nilguntapkan53@gmail.com>
2021-11-21 22:05:23 +0100

    first lesson

--- class1.py
+++ class1.py
@@ -1,3 +1,106 @@
 # Variables 
-message = "Hello World People!"
-print(message)
+# message = "Well, new one!"
+# print(message)
+
+# Naming Rules
+#Can : Letters, Numbers, Underscores
+# message = "25 is here."
+
+# # Can't: 
+# # - Start w numbers 
+# # 2message
+# # - No spaces
+# # message 1
+# # - No keywords
+# # sadece if , int,.. kullanamazsin cunku python da bunlar anahtar kelime. ama int1,if_our,.. yazabilirsin. Bunlar valiebles.
+
+ # Best Practices:
+ # - Short but descriptive
+ # h = "Hello World!" / Bu sekilde kisa baslamasa daha iyi.
+ #h_message = "Hello World!"
+
+ # NameError
+#  print(helo)
+
+ # Exercize
+ # 1
+ # 2
+
+ # Strings
+ #ch = 'a'
+ #ch = "a"
+
+ #desc = ' My name'
+# print(type(desc))
+
+# # Functions
+#name = 'nilGun tApkan olcuM'
+
+# print(name.title()) # Bas harfler buyuk
+# print(name.upper()) # Tum harfler buyuk
+# print(name.lower()) # Tum harfler kucuk
+
+# String concatenation
+# first_name = 'Nilgun'
+# last_name = 'Tapkan Olcum'
+
+# print(first_name + ' ' + last_name)
+
+# full_name = first_name + ' ' + last_name
+# print(full_name.title())
+
+# Whitespace
+# print('Hello World!')
+# # \t - tab ,ikili bosuk olusturacak 
+# print('Hello\tWorld!')
+# # \t - t is never shown!
+# print(' \tea')
+# # \n - new line, alt satira yazacak ikinci kelimeyi.
+# print('Hello\nWorld!')
+
+# Stripping Whitespaces
+# name = '    Nil '
+# last_name = 'To'
+# full_name = name + last_name
+# print(name.lstrip() + last_name)
+# # # left strip
+# print(name.rstrip() + last_name)
+# # strip
+# print(name.strip() + last_name)
+# print(name.rstrip().lstrip() + last_name)
+
+# print(full_name + '-')
+
+# Number
+# Integer
+# print(3 + 2)
+# print(3 - 2)
+# print(5 * 2)
+# print( 4 / 2)
+
+# #power
+# print(5 ** 2)
+
+# calc = 5 + 2 * 2
+# print(calc)
+
+# calc = (5 +2) * 2
+# print(calc)
+
+# Floating-point numbers
+# print(0.1 + 0.1)
+
+# print(0.1 + 0.2)
+
+# COMMENTS
+# is a comment this line operator
+'''
+This
+is 
+a 
+multiline
+comment
+'''
+
+#2
+
